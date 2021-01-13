0e4b326b2054f245cb2268590dca4a98eb27c29a
Daniel Morais Borges <daniel.morais@bumlai.com.br>
2021-01-13 09:37:43 -0400

    Orders Challenge 09 ready

--- src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
@@ -4,6 +4,7 @@ import IProductsRepository from '@modules/products/repositories/IProductsReposit
 import ICreateProductDTO from '@modules/products/dtos/ICreateProductDTO';
 import IUpdateProductsQuantityDTO from '@modules/products/dtos/IUpdateProductsQuantityDTO';
 import Product from '../entities/Product';
+import { INSPECT_MAX_BYTES } from 'buffer';
 
 interface IFindProducts {
   id: string;
@@ -22,20 +23,46 @@ class ProductsRepository implements IProductsRepository {
     quantity,
   }: ICreateProductDTO): Promise<Product> {
     // TODO
+    const product = await this.ormRepository.create({
+      name,
+      price,
+      quantity,
+    })
+
+    await this.ormRepository.save(product);
+
+    return product;
   }
 
   public async findByName(name: string): Promise<Product | undefined> {
     // TODO
+    const product = await this.ormRepository.findOne({
+      where: {
+        name,
+      },
+    });
+
+    return product;
   }
 
   public async findAllById(products: IFindProducts[]): Promise<Product[]> {
     // TODO
+    const productIds = products.map(product => product.id);
+
+    const existentProducts = await this.ormRepository.find({
+      where: {
+        id: In(productIds),
+      },
+      });
+
+    return existentProducts;
   }
 
   public async updateQuantity(
     products: IUpdateProductsQuantityDTO[],
   ): Promise<Product[]> {
     // TODO
+    return this.ormRepository.save(products);
   }
 }
 
---
+++ src/shared/infra/typeorm/migrations/1610392765395-AddCustomerIdToOrders.ts
@@ -0,0 +1,35 @@
+import { query } from "express";
+import {MigrationInterface, QueryRunner, TableColumn, TableForeignKey} from "typeorm";
+import { MongoQueryRunner } from "typeorm/driver/mongodb/MongoQueryRunner";
+
+export default class AddCustomerIdToOrders1610392765395 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+
+      await queryRunner.addColumn(
+        'orders',
+        new TableColumn({
+          name: 'customer_id',
+          type: 'uuid',
+          isNullable: true,
+        }),
+      );
+
+      await queryRunner.createForeignKey(
+        'orders',
+        new TableForeignKey({
+          name: 'OrdersCustomer',
+          columnNames: ['customer_id'],
+          referencedColumnNames: ['id'],
+          referencedTableName: 'customers',
+          onDelete: 'SET NULL',
+        }),
+      );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropForeignKey('orders','ordersCustomer');
+      await queryRunner.dropColumn('orders','customer_id');
+    }
+
+}
